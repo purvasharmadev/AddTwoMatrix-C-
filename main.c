c336cf93cefa0e421f9fc7a2295a83470fee6989
Purva <purva08815294379@gmail.com>
2020-09-27 12:32:56 +0530

    AddTwoMatrix

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int A[3][3],B[3][3],C[3][3],i,j;
+    printf("Enter 9 num for first matrix\n");
+    for(i=0;i<=2;i++){
+        for(j=0;j<=2;j++){
+            scanf("%d",&A[i][j]);
+        }
+    }
+    printf("Enter 9 num for second matrix\n");
+    for(i=0;i<=2;i++){
+        for(j=0;j<=2;j++){
+            scanf("%d",&B[i][j]);
+        }
+    }
+    printf("Sum of the matrix A and B are\n");
+    for(i=0;i<=2;i++)
+    {
+        for(j=0;j<=2;j++)
+            {
+
+            C[i][j]=A[i][j]+B[i][j];
+            printf("%d\t",C[i][j]);
+
+            }
+        printf("\n");
+    }
+    return 0;
+}
